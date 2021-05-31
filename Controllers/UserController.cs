38ff7ef88a14d1d9d8536f40b11c070dbb29cc09
pjbatz16 <78991855+pjbatz16@users.noreply.github.com>
2021-05-31 23:52:39 +0800

    Add files via upload

---
+++ Controllers/UserController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC_CRUD.DataContext;
+using MVC_CRUD.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC_CRUD.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly CrudDBContext _cruddbcontext; 
+
+        public UserController(CrudDBContext crudDBContext)
+        {
+            _cruddbcontext = crudDBContext;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register(User u)
+        {
+            if (ModelState.IsValid)
+            {
+                //no errors in form
+                //insert the record to DB via EF Core
+                _cruddbcontext.Users.Add(u);
+                _cruddbcontext.SaveChanges();
+                return RedirectToAction("Allusers");
+            }
+            return View("Index");
+        }
+
+        public IActionResult Allusers()
+        {
+            return View(_cruddbcontext.Users.ToList());
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        //[HttpPost]
+        //public IActionResult Edit(int id)
+        //{
+        //    return View();
+        //} 
+
+        public IActionResult Delete(int id)
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC_CRUD.Models
+{
+    public class User
+    {
+        [Key]
+        public int UserId { get; set; }
+
+        [Required(ErrorMessage ="Please Enter Complete Name!")]
+        public string Name { get; set; }
+
+        [Required]
+        [Range(20,30)]
+        public int Age { get; set; }
+
+        [Required]
+        public string Address { get; set; }
+    }
+}
